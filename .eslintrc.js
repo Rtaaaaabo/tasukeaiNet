f3d632f557c68f9717b0e06a0bda2efc1a799888
Rtaaaaabo <r.taaaaabo@gmail.com>
2022-11-04 12:34:07 +0900

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+require("jest-preset-angular/ngcc-jest-processor");
+
+module.exports = {
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+};
